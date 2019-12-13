f67be090323ea3f9e121281c97dfe3a603cb5a23
niki <o8o71052631@gmail.com>
2019-12-13 16:01:16 +0900

    update data.js

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,5 @@
 $(function() {
-
-  //tagを抽出
+  //tagを抽出------------------
   var arg  = new Object;
   url = location.search.substring(1).split('&');
   for(i=0; url[i]; i++) {
@@ -14,32 +13,62 @@ $(function() {
   else{
     $(".bloglist").append("<p>"+tag+"タグの記事</p>");
   }
+  //tagを抽出------------------
+
+    $.getJSON("./data/data.json", function(dataname){
+        for(var i in dataname){
+          var path ="./data/md/"+ dataname[i].id +".md";
+          var forCount=i;
+          (function(i){
+          $.get(path, function(data, status, xhr) {
+            console.log(path);
+              if (status == "success") {
+
+                var arr = data.split(/\r\n|\r|\n/);
+                var h;  //ブログタイトル
+                for( var p=0; p < arr.length; p++) {
+                  if (!arr[p]) {
+                    continue;
+                  }else{
+                    h = arr[p];
+                    break;
+                  }
+                };
+                h = h.replace( /#/g , "" );
 
 
-    $.getJSON("./data/data.json", function(data){
-        for(var i in data){
-          if(tag === undefined){
-            //全取得
-            $(".bloglist").append(
-              "<li>" +
-                "<a href='./data.html?src=" + data[i].id + "' >" + 
-                  "<img src='http://placehold.it/600x360' alt='"+ data[i].tag +"' />" + 
-                "</a>"+
-                "<span><a href='./blog.html?tag=" + data[i].tag + "' >"+ data[i].tag +"</a></span>" +            
-                "<a href='./data.html?src=" + data[i].id + "' >" + data[i].title + "</a>" +
-              "</li>");
-          }else{
-            //タグ検索
-            if(tag == data[i].tag){
-              $(".bloglist").append(
-              "<li>" +
-                "<a href='./data.html?src=" + data[i].id + "' >" + 
-                  "<img src='http://placehold.it/600x360' alt='"+ data[i].tag +"' />" + 
-                "</a>"+
-                "<a href='./data.html?src=" + data[i].id + "' >" + data[i].title + "</a>" +
-              "</li>");            
-            }              
-          }
+                if(tag === undefined){
+                  //全取得
+                  $(".bloglist").append(
+                    "<li>" +
+                      "<a href='./data.html?src=" + dataname[i].id + "' >" +
+                        "<img src='http://placehold.it/600x360' alt='"+ dataname[i].tag +"' />" +
+                      "</a>"+
+                      "<span><a href='./blog.html?tag=" + dataname[i].tag + "' >"+ dataname[i].tag +"</a></span>" +
+                      "<a href='./data.html?src=" + dataname[i].id + "' >" + h +"</a>" +
+                    "</li>");
+                }else{
+                  //タグ検索
+                  if(tag == dataname[i].tag){
+                    $(".bloglist").append(
+                    "<li>" +
+                      "<a href='./data.html?src=" + dataname[i].id + "' >" +
+                        "<img src='http://placehold.it/600x360' alt='"+ dataname[i].tag +"' />" +
+                      "</a>"+
+                      "<a href='./data.html?src=" + dataname[i].id + "' >" + h +"</a>" +
+                    "</li>");
+                  }
+                }
+
+
+
+
+              }
+          });
+        })(forCount);
+
+
+
 
         }
     });
